bb88913d54f704153c154237dcda8ac5d9e44220
ro77en <lucas.mdantas@icloud.com>
2024-12-05 11:24:35 -0300

    feat: add mysql connection and config.json file

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 // import express module
 const express = require('express');
 
+// import config.json
+const config = require('./config.json');
+
+// import mysql2 moduule
+const mysql = require('mysql2');
+
 // app
 const app = express();
+
+// mysql connection config
+const connection = mysql.createConnection(config.db);
+
+// connection test
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('successfully connected!');
+});
 
 // hello world route
 app.get('/', (req, res) => {
