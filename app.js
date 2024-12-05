4beb7c591098e18b7b3f43087a9cae67f0110552
ro77en <lucas.mdantas@icloud.com>
2024-12-05 09:33:59 -0300

    feat: setup express server + hello world route

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('hello world');
+// import express module
+const express = require('express');
+
+// app
+const app = express();
+
+// hello world route
+app.get('/', (req, res) => {
+    res.write('hello world');
+    res.end();
+});
+
+// server
+app.listen(8080, () => {
+    console.log('server running...');
+});
